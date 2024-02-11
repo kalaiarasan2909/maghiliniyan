516d7e321777426135877a9ed6ea2f19e155c6a2
kalaiarasan2909 <kalaiarasans2909@gmail.com>
2024-02-11 11:56:39 +0530

    message

---
+++ looping.java
@@ -0,0 +1,24 @@
+package com.kalai2;
+
+public class looping {
+	
+	public static void main(String[] args) {
+	
+
+	int b=10;
+	
+	do {
+   System.out.println(b);
+   b++;
+   
+	}
+	while(b<15);
+}
+
+}
+	
+	
+	
+
+// now my code was resolved so we no need to worry about that .at the same time we need to practise a lot then only it will automatically otherwise we can do anything in a proper maner
+
